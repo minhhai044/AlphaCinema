a1596194bf1118a2f3e6b206f6bbd596629b0e2f
huycodedao <phunghuyk24@gmail.com>
2025-02-14 16:19:46 +0700

    feat: Moive api

---
+++ app/Http/Controllers/Api/MovieController.php
@@ -0,0 +1,112 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\MovieRequest;
+use App\Models\Movie;
+use App\Services\MovieService;
+use App\Traits\ApiResponseTrait;
+use Illuminate\Http\Response;
+
+class MovieController extends Controller
+{
+    use ApiResponseTrait;
+
+    protected $movieService;
+
+    public function __construct(MovieService $movieService)
+    {
+        $this->movieService = $movieService;
+    }
+
+
+    public function index()
+    {
+        try {
+            $movies = $this->movieService->getAllMovies();
+            return $this->successResponse(
+                $movies,
+                'Movies retrieved successfully!',
+                Response::HTTP_OK
+            );
+        } catch (\Exception $e) {
+            return $this->errorResponse(
+                $e->getMessage(),
+                Response::HTTP_INTERNAL_SERVER_ERROR
+            );
+        }
+    }
+
+
+    public function show(string $id)
+    {
+        try {
+            $movie = Movie::query()->findOrFail($id);
+            return $this->successResponse(
+                $movie,
+                'Movie retrieved successfully!',
+                Response::HTTP_OK
+            );
+        } catch (\Exception $e) {
+            return $this->errorResponse(
+                $e->getMessage(),
+                Response::HTTP_INTERNAL_SERVER_ERROR
+            );
+        }
+    }
+
+
+    public function store(MovieRequest $request)
+    {
+        try {
+            $data = $this->movieService->createMovie($request->all());
+            return $this->successResponse(
+                $data,
+                'Movie created successfully!',
+                Response::HTTP_CREATED
+            );
+        } catch (\Exception $e) {
+            return $this->errorResponse(
+                $e->getMessage(),
+                Response::HTTP_INTERNAL_SERVER_ERROR
+            );
+        }
+    }
+
+
+    public function update(MovieRequest $request, string $id)
+    {
+        try {
+            $data = $this->movieService->updateMovie($id, $request->all());
+            return $this->successResponse(
+                $data,
+                'Movie updated successfully!',
+                Response::HTTP_OK
+            );
+        } catch (\Exception $e) {
+            return $this->errorResponse(
+                $e->getMessage(),
+                Response::HTTP_INTERNAL_SERVER_ERROR
+            );
+        }
+    }
+
+    
+    public function delete(string $id)
+    {
+        try {
+            $this->movieService->deleteMovie($id);
+            return $this->successResponse(
+                null,
+                'Movie deleted successfully!',
+                Response::HTTP_OK
+            );
+        } catch (\Exception $e) {
+            return $this->errorResponse(
+                $e->getMessage(),
+                Response::HTTP_INTERNAL_SERVER_ERROR
+            );
+        }
+    }
+}
