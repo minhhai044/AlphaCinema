248dee4745977fd75e7dc21f00d7cfd71d312143
minhhai044 <tmhai2004@gmail.com>
2025-02-25 14:11:45 +0700

    Feat : Confix route api

--- routes/version/v1.php
+++ routes/version/v1.php
@@ -15,20 +15,20 @@ use Illuminate\Support\Facades\Route;
 // Route::post('/login', [AuthController::class, 'login']);
 // Route::post('/register', [AuthController::class, 'register']);
 
-Route::post('signin', [AuthController::class, 'signIn'])->name('api.users.signin');
-Route::post('signup', [AuthController::class, 'signUp'])->name('api.users.signup');
+Route::post('signin', [AuthController::class, 'signIn']);
+Route::post('signup', [AuthController::class, 'signUp']);
+Route::get('{id}/showtime',[ShowtimeController::class, 'getByDate']);
+Route::get('{slug}/movieShowTimes',[ShowtimeController::class, 'movieShowTimes']);
+Route::get('/listMovies',[ShowtimeController::class, 'listMovies']);
+Route::get('{movie}/listShowtimes',[ShowtimeController::class, 'listShowtimes']);
+Route::get('{slug}/showtimeDetail',[ShowtimeController::class, 'showtimeDetail']);
 
 Route::middleware('auth:sanctum')->group(function () {
+    
     Route::post('/logout', [AuthController::class, 'logout']);
 
-    Route::get('{slug}/movieShowTimes',     [ShowtimeController::class, 'movieShowTimes']);
-
-
-    Route::get('{id}/showtime',             [ShowtimeController::class, 'getByDate']);
     Route::put('{id}/active-showtime',      [ShowtimeController::class, 'activeShowtime']);
-    Route::get('/listMovies',               [ShowtimeController::class, 'listMovies']);
-    Route::get('{movie}/listShowtimes',     [ShowtimeController::class, 'listShowtimes']);
-    Route::get('{slug}/showtimeDetail',     [ShowtimeController::class, 'showtimeDetail']);
+    
     Route::post('{id}/changeSeatStatus',    [ShowtimeController::class, 'changeSeatStatus']);
 
     Route::put('{id}/active-seat-template', [SeatTemplateController::class, 'activeSeatTemplate']);
