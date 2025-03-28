91bb71fecdc739e58533ba87333f41a7c5d07e5a
minhhai044 <tmhai2004@gmail.com>
2025-03-29 00:17:51 +0700

    Fix : Clean Route By Minh Hải

--- routes/version/v1.php
+++ routes/version/v1.php
@@ -1,10 +1,6 @@
 <?php
 
-use App\Http\Controllers\Admin\AuthController as AdminAuthController;
-use App\Http\Controllers\Admin\MovieController;
 use App\Http\Controllers\Admin\RankController;
-use App\Http\Controllers\Admin\TicketController as AdminTicketController;
-use App\Http\Controllers\Api\PointHistoryController;
 use App\Http\Controllers\Api\SiteSettingController;
 use App\Http\Controllers\Api\UserController;
 use App\Http\Controllers\Api\V1\AuthController;
@@ -14,12 +10,9 @@ use App\Http\Controllers\Api\V1\ComboFoodController;
 use App\Http\Controllers\Api\V1\PaymentController;
 use App\Http\Controllers\Api\V1\RoomController;
 use App\Http\Controllers\Api\V1\SeatTemplateController;
-use App\Http\Controllers\Api\V1\SendMailController;
 use App\Http\Controllers\Api\V1\ShowtimeController;
-
 use App\Http\Controllers\Api\V1\SlideShowController;
 use App\Http\Controllers\Api\V1\TicketController;
-use App\Http\Controllers\Api\VoucherController;
 use Illuminate\Support\Facades\Route;
 
 
@@ -27,30 +20,61 @@ Route::prefix('users')->group(function () {
     Route::get('/', [UserController::class, 'index'])->name('api.users.index');
 });
 
-// Route::get('{branch}/listMovies/{cinema}', [ShowtimeController::class, 'listMovies']);
-
+/**
+ * Route Api Auth
+ * 
+ * signin : Đăng nhập
+ * 
+ * signup : Đăng ký tài khoản
+ */
 
 Route::post('signin', [AuthController::class, 'signIn']);
 Route::post('signup', [AuthController::class, 'signUp']);
 
+/**
+ * Route Api showtime
+ * 
+ * showtime : Lấy danh sách suất chiếu theo ngày
+ * 
+ * {slug}/movieShowTimes : Chi tiết showtime theo phim By Slug
+ * 
+ * listMovies : Danh sách movie
+ * 
+ * {slug}/showtimeDetail : Xem chi tiết suất chiếu
+ */
 
-
-Route::get('{id}/showtime', [ShowtimeController::class, 'getByDate']);
+Route::get('{id}/showtime',         [ShowtimeController::class, 'getByDate']);
 Route::get('{slug}/movieShowTimes', [ShowtimeController::class, 'movieShowTimes']);
-Route::get('/listMovies', [ShowtimeController::class, 'listMovies']);
+Route::get('/listMovies',           [ShowtimeController::class, 'listMovies']);
 Route::get('{movie}/listShowtimes', [ShowtimeController::class, 'listShowtimes']);
 Route::get('{slug}/showtimeDetail', [ShowtimeController::class, 'showtimeDetail']);
 
+/**
+ * Api slideshows
+ * 
+ * slideshows : Lấy image slide
+ */
 Route::get('/slideshows', [SlideShowController::class, 'index']);
 
-Route::prefix('branchs')->group(function () {
-    Route::get('/', [BranchController::class, 'index']);
-});
 
-Route::get('/get-cinemas', [TicketController::class, 'getCinemas']);
-
-
+/**
+ * Api branchs
+ * 
+ * branchs : Lấy danh sách chi nhánh + rạp
+ * 
+ * get-cinemas : Lấy danh sách rạp
+ * 
+ */
+Route::get('/branchs',      [BranchController::class, 'index']);
+Route::get('/get-cinemas',  [TicketController::class, 'getCinemas']);
+/**
+ * Api rank
+ * 
+ * ranksJson : Lấy rank
+ * 
+ */
 Route::get('/ranksJson', [RankController::class, 'getRanksJson']);
+
 Route::middleware('auth:sanctum')->group(function () {
 
     Route::post('tickets', [TicketController::class, 'createTicket']);
@@ -63,19 +87,15 @@ Route::middleware('auth:sanctum')->group(function () {
 
     Route::get('/ticket-by-user', [TicketController::class, 'getTicketByUser']);
     Route::get('/voucher', [AuthController::class, 'getUserVoucher']);
-    // Route::prefix('point_histories')->group(function () {
-    //     Route::get('/', [PointHistoryController::class, 'index']);
-    //     Route::get('{id}', [PointHistoryController::class, 'show']);
-    // });
+   
     Route::post('{id}/changeSeatStatus',    [ShowtimeController::class, 'changeSeatStatus']);
 
-    // Route::put('{id}/active-seat-template', [SeatTemplateController::class, 'activeSeatTemplate']);
-
-    // Route::put('{id}/active-room',          [RoomController::class, 'activeRoom']);
+   
 
     Route::post('{id}/resetSuccessSeat',     [ShowtimeController::class, 'resetSuccessSeat']);
-    // Route::put('{id}/resetSuccessSeat',     [ShowtimeController::class, 'resetSuccessSeat']);
+    
 });
+
 Route::put('{id}/active-seat-template', [SeatTemplateController::class, 'activeSeatTemplate']);
 Route::put('{id}/active-room',          [RoomController::class, 'activeRoom']);
 Route::get('/foods', [FoodController::class, 'index']);
