52eec7d1405c8e61b6a840c822fb08d6f1b8335f
minhhai044 <tmhai2004@gmail.com>
2025-03-29 22:40:00 +0700

    Fix : Un Delete Voucher By Minh Hải

--- resources/views/admin/layouts/partials/script.blade.php
+++ resources/views/admin/layouts/partials/script.blade.php
@@ -39,6 +39,5 @@
 
 <script src="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.13/js/select2.min.js"></script>
 
-<script src="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.13/js/select2.min.js"></script>
 
 
