ec331d885dd1c8ddb3f728ca56d7f7c3fd69fc42
tglam03 <nguyenductunglam94@gmail.com>
2025-04-10 22:41:22 +0700

    fix combo alert

--- resources/views/admin/combos/index.blade.php
+++ resources/views/admin/combos/index.blade.php
@@ -125,7 +125,18 @@
     <script src="{{ asset('theme/admin/assets/js/pages/datatables.init.js') }}"></script>
 
     <script>
-      
+        // Hàm xác nhận trước khi thay đổi
+        function confirmChange(text = 'Bạn có chắc chắn muốn thay đổi trạng thái Combo?', title =
+            'AlphaCinema thông báo') {
+            return Swal.fire({
+                icon: 'warning',
+                title: title,
+                text: text,
+                showCancelButton: true,
+                confirmButtonText: 'Đồng ý',
+                cancelButtonText: 'Hủy',
+            }).then(result => result.isConfirmed);
+        }
 
         // Gắn sự kiện thay đổi trạng thái
         $(document).on("change", ".changeActive", function(e) {
