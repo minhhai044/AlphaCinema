4e88b10ba99d22ada753da75ca65e90ae66fd485
minhhai044 <tmhai2004@gmail.com>
2025-04-17 08:27:35 +0700

    Fix User Voucher by Minh Hải

--- resources/views/admin/user_vouchers/edit.blade.php
+++ resources/views/admin/user_vouchers/edit.blade.php
@@ -33,11 +33,11 @@
 <div class="row">
     <div class="col-12">
         <div class="page-title-box d-sm-flex align-items-center justify-content-between">
-            <h4 class="mb-sm-0 font-size-18">Chỉnh sửa User Voucher</h4>
+            <h4 class="mb-sm-0 font-size-18">Chỉnh sửa Voucher người dùng</h4>
             <div class="page-title-right">
                 <ol class="breadcrumb m-0">
                     <li class="breadcrumb-item">
-                        <a href="{{ route('admin.user-vouchers.index') }}">User Voucher</a>
+                        <a href="{{ route('admin.user-vouchers.index') }}">Voucher người dùng</a>
                     </li>
                     <li class="breadcrumb-item active">Chỉnh sửa</li>
                 </ol>
