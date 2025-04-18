8d2d4107ca934289852ecad9645f95f55be549e9
tglam03 <nguyenductunglam94@gmail.com>
2025-04-18 22:20:02 +0700

    fix validate user

--- app/Http/Requests/UserRequest.php
+++ app/Http/Requests/UserRequest.php
@@ -38,13 +38,19 @@ class UserRequest extends FormRequest
     {
         return [
             'name' => 'required|string|max:255',
-            'avatar' => 'nullable|image|mimes:jpeg,png,jpg|max:2048',
+            'avatar' => 'nullable|image|mimes:jpeg,png,jpg,gif,svg|max:2048',
             'phone' => 'required|regex:/^\+?[0-9]{10,15}$/|unique:users,phone',  // Kiểm tra số điện thoại duy nhất
-            'email' => 'required|email|unique:users,email',  // Kiểm tra email duy nhất
+            // 'email' => 'required|email|unique:users,email',  // Kiểm tra email duy nhất
+            'email' => [
+                'required',
+                'unique:users,email',
+                'regex:/^[\w\.\-]+@([\w\-]+\.)+[a-zA-Z]{2,}$/'
+            ],
+
             'password' => 'required|string|min:8|confirmed',
             'address' => 'nullable|string|max:255',
             'gender' => 'nullable',
-            'birthday' => 'nullable|date',
+            'birthday' => 'nullable|date|before_or_equal:today',
             'total_amount' => 'nullable|numeric',  // Nếu cần kiểu dữ liệu số cho 'total_amount'
             'type_user' => 'boolean',
             'cinema_id' => 'nullable|exists:cinemas,id',
@@ -56,13 +62,20 @@ class UserRequest extends FormRequest
     {
         return [
             'name' => 'required|string|max:255',
-            'avatar' => 'nullable',
+            // 'avatar' => 'nullable',
+            'avatar' => 'nullable|image|mimes:jpeg,png,jpg,gif,svg|max:2048',
             'phone' => 'nullable|regex:/^\+?[0-9]{10,15}$/|unique:users,phone,' . $userId,  // Bỏ qua kiểm tra số điện thoại nếu là bản ghi hiện tại
-            'email' => 'required|email|unique:users,email,' . $userId,  // Bỏ qua kiểm tra email nếu là bản ghi hiện tại
+            // 'email' => 'required|email|unique:users,email,' . $userId,  // Bỏ qua kiểm tra email nếu là bản ghi hiện tại
+            'email' => [
+                'required',
+                'email',
+                'unique:users,email,' . $userId,
+                'regex:/^[\w\.\-]+@([\w\-]+\.)+[a-zA-Z]{2,}$/'
+            ],
             'password' => 'nullable|string|min:8|confirmed',  // Mật khẩu có thể không thay đổi
             'address' => 'nullable|string|max:255',
             'gender' => 'boolean',
-            'birthday' => 'nullable|date',
+            'birthday' => 'nullable|date|before_or_equal:today',
             'total_amount' => 'nullable|numeric',  // Kiểm tra dữ liệu số cho 'total_amount'
             'type_user' => 'boolean',
             'cinema_id' => 'nullable|exists:cinemas,id',
@@ -85,16 +98,36 @@ class UserRequest extends FormRequest
             'name.string' => 'Tên phải là một chuỗi ký tự.',
             'name.max' => 'Tên không được vượt quá 255 ký tự.',
 
-            'avatar.image' => 'Ảnh đại diện phải là một file ảnh.',
-            'avatar.max' => 'Ảnh đại diện không được vượt quá 2MB.',
+            // 'avatar.image' => 'Ảnh đại diện phải là một file ảnh.',
+            // 'avatar.max' => 'Ảnh đại diện không được vượt quá 2MB.',
+            // 'avatar' => [
+            //     'image' => 'Tệp tải lên phải là một hình ảnh.',
+            //     'mimes' => 'Hình ảnh phải có định dạng: jpeg, png, jpg, gif, svg.',
+            //     'max' => 'Hình ảnh không được vượt quá 2MB.',
+            //     'url' => 'Hình ảnh phải là một URL hợp lệ.'
+            // ],
+
+            'avatar' => [
+                'required' => 'Vui lòng chọn hình ảnh.',
+                'image' => 'Tệp tải lên phải là một hình ảnh.',
+                'mimes' => 'Hình ảnh phải có định dạng: jpeg, png, jpg, gif, svg.',
+                'max' => 'Hình ảnh không được vượt quá 2MB.',
+                'url' => 'Hình ảnh phải là một URL hợp lệ.'
+            ],
 
             'phone.required' => 'Số điện thoại là bắt buộc',
             'phone.regex' => 'Số điện thoại không hợp lệ. Vui lòng nhập lại.',
             'phone.unique' => 'Số điện thoại này đã được đăng ký. Vui lòng chọn một số khác.',  // Thông báo lỗi cho số điện thoại trùng
 
-            'email.required' => 'Email là bắt buộc.',
-            'email.email' => 'Email phải là một địa chỉ email hợp lệ.',
-            'email.unique' => 'Email đã tồn tại. Vui lòng chọn một email khác.',  // Thông báo lỗi cho email trùng
+            // 'email.required' => 'Email là bắt buộc.',
+            // 'email.email' => 'Email phải là một địa chỉ email hợp lệ.',
+            // 'email.unique' => 'Email đã tồn tại. Vui lòng chọn một email khác.',  // Thông báo lỗi cho email trùng
+
+            'email.required' => 'Vui lòng nhập email',
+            'email.email' => 'Email phải đúng định dạng (ví dụ: yourname@example.com)',
+            'email.unique' => 'Email này đã tồn tại',
+            'email.regex' => 'Email không đúng định dạng (phải có @ và tên miền đầy đủ)',
+
 
             'password.required' => 'Mật khẩu là bắt buộc.',
             'password.string' => 'Mật khẩu phải là một chuỗi ký tự.',
@@ -108,6 +141,7 @@ class UserRequest extends FormRequest
             'gender.boolean' => 'Giới tính phải là một giá trị boolean hợp lệ.',
 
             'birthday.date' => 'Ngày sinh phải là một ngày hợp lệ.',
+            'birthday.before_or_equal' => 'Ngày sinh không được là ngày trong tương lai.',
 
             'type_user.boolean' => 'Loại người dùng phải là một giá trị boolean hợp lệ.',
 
